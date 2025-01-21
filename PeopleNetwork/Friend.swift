5e98fbe81fb5505bafd7ae2ebf0afea923871b3a
Lochoa11 <linochoa11690@gmail.com>
2025-01-21 10:27:42 -0500

    Added init for both user and friend models, app is still working

--- PeopleNetwork/Friend.swift
+++ PeopleNetwork/Friend.swift
@@ -6,13 +6,26 @@
 //
 
 import Foundation
+import SwiftData
 
-struct Friend: Decodable, Identifiable, Hashable {
+@Model
+class Friend: Decodable {
     let id: UUID
     var name: String
     
     init(id: UUID, name: String) {
         self.id = id
         self.name = name
+    }
+    
+    required init(from decoder: any Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.id = try container.decode(UUID.self, forKey: .id)
+        self.name = try container.decode(String.self, forKey: .name)
+    }
+    
+    enum CodingKeys: String, CodingKey {
+        case id
+        case name
     }
 }
--- PeopleNetwork/User.swift
+++ PeopleNetwork/User.swift
@@ -5,9 +5,11 @@
 //  Created by Lin Ochoa on 1/17/25.
 //
 
+import SwiftData
 import SwiftUI
 
-struct User: Decodable, Identifiable, Hashable {    
+@Model
+class User: Decodable {
     let id: UUID
     var isActive: Bool
     var name: String
@@ -34,4 +36,32 @@ struct User: Decodable, Identifiable, Hashable {
         self.friends = friends
     }
     
+    required init(from decoder: any Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.id = try container.decode(UUID.self, forKey: .id)
+        self.isActive = try container.decode(Bool.self, forKey: .isActive)
+        self.name = try container.decode(String.self, forKey: .name)
+        self.age = try container.decode(Int.self, forKey: .age)
+        self.company = try container.decode(String.self, forKey: .company)
+        self.email = try container.decode(String.self, forKey: .email)
+        self.address = try container.decode(String.self, forKey: .address)
+        self.about = try container.decode(String.self, forKey: .about)
+        self.registered = try container.decode(Date.self, forKey: .registered)
+        self.tags = try container.decode([String].self, forKey: .tags)
+        self.friends = try container.decode([Friend].self, forKey: .friends)
+    }
+    
+    enum CodingKeys: String, CodingKey {
+        case id
+        case isActive
+        case name
+        case age
+        case company
+        case email
+        case address
+        case about
+        case registered
+        case tags
+        case friends
+    }
 }
